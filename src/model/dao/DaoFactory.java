e3927d638ca29bdc1d9ef90989a5bad9cbedc90a
Felipe <Felipesss.2014@gmail.com>
2023-07-04 20:46:51 -0300

    Implementacoes na DepartamentDao

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,16 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartamentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSllerDao() {
 		return new SellerDaoJDBC(DB.getConection());
+	}
+	public static DepartamentDao createDepartmentDao() {
+		return new DepartamentDaoJDBC(DB.getConection());
 	}
 
 }
